b046f9451730794d3e1d26e3f36644339bc6cea3
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-04-07 23:24:21 +0330

    chore: change the app name and package name

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.momentum_track"
+    namespace = "dev.momentum.track"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.momentum_track"
+        applicationId = "dev.momentum.track"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/dev/momentum/track/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.momentum.track
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
