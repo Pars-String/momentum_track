c105a5a430f382e352b9fd8adfae0748aa08f895
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-04-09 00:21:28 +0330

    ref: update the app version to 1.0.1

--- lib/features/projects/presentation/screens/projects_screen.dart
+++ lib/features/projects/presentation/screens/projects_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:momentum_track/core/constant/app_constant.dart';
 import 'package:momentum_track/core/resources/app_routes.dart';
 import 'package:momentum_track/core/widgets/app_modal_bottom_sheet.dart';
 import 'package:momentum_track/features/projects/presentation/bloc/projects_bloc.dart';
@@ -71,7 +72,7 @@ class ProjectsScreen extends StatelessWidget {
                     padding: const EdgeInsets.only(bottom: 15.0, top: 8),
 
                     child: Text(
-                      'DEV 1.0.0',
+                      'DEV ${AppConstant.appVersion}',
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         color: Theme.of(context).colorScheme.onPrimary,
