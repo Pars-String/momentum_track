b2e29af7c583cad9a3f6cc89bf31f0e4350e52ac
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-04-12 03:08:45 +0330

    feat: implement menu cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:momentum_track/core/resources/app_routes.dart';
 import 'package:momentum_track/core/theme/app_theme.dart';
+import 'package:momentum_track/features/main/presentation/cubit/menu_cubit.dart';
 import 'package:momentum_track/locator.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -10,16 +12,16 @@ void main() async {
   await locatorSetup();
 
   WindowOptions windowOptions = WindowOptions(
-    minimumSize: const Size(800, 600),
+    minimumSize: const Size(850, 600),
     // maximumSize: const Size(800, 600),
-    size: Size(800, 600),
+    size: Size(850, 600),
   );
   windowManager.waitUntilReadyToShow(windowOptions, () async {
     await windowManager.show();
     await windowManager.focus();
   });
 
-  runApp(const MyApp());
+  runApp(BlocProvider(create: (context) => MenuCubit(), child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
