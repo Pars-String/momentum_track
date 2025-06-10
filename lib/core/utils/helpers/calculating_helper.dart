5e1b9a321ff7b299d23bcdd6d0874f73df84aa07
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-06-10 03:57:15 +0330

    feat: move calculate duration method to isolate thread

--- lib/core/utils/helpers/calculating_helper.dart
+++ lib/core/utils/helpers/calculating_helper.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:momentum_track/core/database/app_database.dart';
 import 'package:shamsi_date/shamsi_date.dart';
 
@@ -14,14 +15,12 @@ class CalculatingHelper {
     return diff.inMinutes / 60.0;
   }
 
-  static Duration calculateDurationFrom(List<TimeEntry> timeEntries) {
-    Duration totalDuration = Duration.zero;
-    for (var timeEntry in timeEntries) {
-      totalDuration +=
-          timeEntry.duration != null
-              ? convertDoubleToDuration(timeEntry.duration!)
-              : Duration.zero;
-    }
+  static Future<Duration> calculateDurationFrom(
+    List<TimeEntry> timeEntries,
+    int projectID,
+  ) async {
+    final args = (timeEntries, projectID);
+    Duration totalDuration = await compute(_calculateDurationFrom, args);
 
     return totalDuration;
   }
@@ -125,4 +124,23 @@ List<Jalali> _getJalaliMonthDates(Jalali date) {
     daysInMonth,
     (i) => Jalali(date.year, date.month, i + 1),
   );
+}
+
+Duration _calculateDurationFrom((List<TimeEntry>, int) args) {
+  final timeEntries = args.$1;
+  final projectID = args.$2;
+  final listedEntries =
+      timeEntries
+          .where((timeEntry) => timeEntry.projectId == projectID)
+          .toList();
+
+  Duration totalDuration = Duration.zero;
+  for (var timeEntry in listedEntries) {
+    totalDuration +=
+        timeEntry.duration != null
+            ? Duration(seconds: (timeEntry.duration! * 3600).round())
+            : Duration.zero;
+  }
+
+  return totalDuration;
 }
