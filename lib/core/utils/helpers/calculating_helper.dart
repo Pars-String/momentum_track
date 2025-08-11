3b72a5d5421c7ef1a4b5f144c1f3ebced06978f6
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-08-11 16:52:51 +0330

    feat: implement last month day model and config on calculating helper

--- lib/core/utils/helpers/calculating_helper.dart
+++ lib/core/utils/helpers/calculating_helper.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/foundation.dart';
-import 'package:momentum_track/core/database/app_database.dart';
+import 'package:momentum_track/core/data/models/last_month_day.dart';
 import 'package:shamsi_date/shamsi_date.dart';
 
 class CalculatingHelper {
@@ -13,16 +12,6 @@ class CalculatingHelper {
 
     // Convert duration to double (hours)
     return diff.inMinutes / 60.0;
-  }
-
-  static Future<Duration> calculateDurationFrom(
-    List<TimeEntry> timeEntries,
-    int projectID,
-  ) async {
-    final args = (timeEntries, projectID);
-    Duration totalDuration = await compute(_calculateDurationFrom, args);
-
-    return totalDuration;
   }
 
   static Duration convertDoubleToDuration(double hours) {
@@ -40,51 +29,7 @@ class CalculatingHelper {
     );
   }
 
-  static List<DateTime> calculateWeekDates(DateTime? selectedDate) {
-    final DateTime now = selectedDate ?? DateTime.now();
-    final List<DateTime> dateList = [];
-    final DateTime startOfWeek = now.subtract(
-      Duration(days: (now.weekday - 1)),
-    );
-
-    int i = 0;
-    while (i < 7) {
-      final DateTime date = startOfWeek.add(Duration(days: i));
-      dateList.add(
-        date.copyWith(
-          hour: 0,
-          minute: 0,
-          second: 0,
-          millisecond: 0,
-          microsecond: 0,
-        ),
-      );
-      i++;
-    }
-
-    return dateList;
-  }
-
-  static ({List<DateTime> gregorianDates, List<DateTime> jalaliDates})
-  calculateMonthDates(DateTime? selectedDate) {
-    final DateTime now = selectedDate ?? DateTime.now();
-
-    final Gregorian currentGregorian = Gregorian.fromDateTime(now);
-    final List<Gregorian> gregorianDays = _getGregorianMonthDates(
-      currentGregorian,
-    );
-
-    final Jalali currentJalali = currentGregorian.toJalali();
-    final List<Jalali> jalaliDays = _getJalaliMonthDates(currentJalali);
-
-    return (
-      gregorianDates: gregorianDays.map((d) => d.toDateTime()).toList(),
-      jalaliDates: jalaliDays.map((d) => d.toDateTime()).toList(),
-    );
-  }
-
-  static ({DateTime gregorianLastDay, DateTime jalaliLastDay})
-  calculateLastDayOfMonth(DateTime? selectedDate) {
+  static LastMonthDay calculateLastDayOfMonth(DateTime? selectedDate) {
     final DateTime now = selectedDate ?? DateTime.now();
 
     final Gregorian currentGregorian = Gregorian.fromDateTime(now);
@@ -93,54 +38,17 @@ class CalculatingHelper {
     final Jalali currentJalali = currentGregorian.toJalali();
     final int daysInJalaliMonth = currentJalali.monthLength;
 
-    return (
-      gregorianLastDay:
-          Gregorian(
-            currentGregorian.year,
-            currentGregorian.month,
-            daysInGregorianMonth,
-          ).toDateTime(),
-      jalaliLastDay:
-          Jalali(
-            currentJalali.year,
-            currentJalali.month,
-            daysInJalaliMonth,
-          ).toDateTime(),
+    return LastMonthDay(
+      gregorianLastDay: Gregorian(
+        currentGregorian.year,
+        currentGregorian.month,
+        daysInGregorianMonth,
+      ).toDateTime(),
+      jalaliLastDay: Jalali(
+        currentJalali.year,
+        currentJalali.month,
+        daysInJalaliMonth,
+      ).toDateTime(),
     );
   }
-}
-
-List<Gregorian> _getGregorianMonthDates(Gregorian date) {
-  final int daysInMonth = date.monthLength;
-  return List.generate(
-    daysInMonth,
-    (i) => Gregorian(date.year, date.month, i + 1),
-  );
-}
-
-List<Jalali> _getJalaliMonthDates(Jalali date) {
-  final int daysInMonth = date.monthLength;
-  return List.generate(
-    daysInMonth,
-    (i) => Jalali(date.year, date.month, i + 1),
-  );
-}
-
-Duration _calculateDurationFrom((List<TimeEntry>, int) args) {
-  final timeEntries = args.$1;
-  final projectID = args.$2;
-  final listedEntries =
-      timeEntries
-          .where((timeEntry) => timeEntry.projectId == projectID)
-          .toList();
-
-  Duration totalDuration = Duration.zero;
-  for (var timeEntry in listedEntries) {
-    totalDuration +=
-        timeEntry.duration != null
-            ? Duration(seconds: (timeEntry.duration! * 3600).round())
-            : Duration.zero;
-  }
-
-  return totalDuration;
 }
