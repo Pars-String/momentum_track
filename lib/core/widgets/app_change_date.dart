bc2c4551f2684e31123c0764cf4e078a513fb946
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-08-20 14:34:23 +0330

    ref: change the style on app bar and add more details

--- lib/core/widgets/app_change_date.dart
+++ lib/core/widgets/app_change_date.dart
@@ -1,41 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gap/gap.dart';
 import 'package:hugeicons/hugeicons.dart';
-import 'package:intl/intl.dart';
 import 'package:momentum_track/core/bloc/global_date_cubit/global_date_cubit.dart';
+import 'package:momentum_track/core/utils/extensions/date_formatter_extension.dart';
 import 'package:momentum_track/core/utils/helpers/date_helper.dart';
 
 class AppChangeDate extends StatelessWidget {
   final GlobalDateState state;
   const AppChangeDate(this.state, {super.key});
 
+  String get selectedMonth => state.thisMonthDates.first.monthTitle!;
+
+  String get nextMonth {
+    final selectedDate = state.thisMonthDates.first;
+    return selectedDate
+        .copyWith(month: selectedDate.month + 1)
+        .shortMonthTitle!;
+  }
+
+  String get previousMonth {
+    final selectedDate = state.thisMonthDates.first;
+    return selectedDate
+        .copyWith(month: selectedDate.month - 1)
+        .shortMonthTitle!;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final String monthTitle =
-        '${DateFormat('MMMM').format(state.thisMonthDates.first)} ${state.thisMonthDates.first.year}';
     final DateTime now = DateHelper.today();
 
     return Row(
       children: [
-        IconButton(
+        TextButton(
           onPressed: () {
             context.read<GlobalDateCubit>().setNextMonthDates();
           },
-          icon: HugeIcon(
-            icon: HugeIcons.strokeRoundedArrowLeftDouble,
-            color: Theme.of(context).colorScheme.primaryContainer,
+          child: Row(
+            children: [
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedArrowLeftDouble,
+                color: Theme.of(context).colorScheme.primaryContainer,
+              ),
+              Text(nextMonth, style: TextStyle(fontSize: 8)),
+            ],
           ),
         ),
-        Text(monthTitle, style: TextStyle(fontSize: 25)),
-        IconButton(
+
+        Gap(8),
+        SizedBox(
+          width: 180,
+          child: Center(
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(text: selectedMonth, style: TextStyle(fontSize: 25)),
+                  TextSpan(
+                    text: '\n${state.thisMonthDates.first.year}',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.right,
+            ),
+          ),
+        ),
+        Gap(8),
+
+        TextButton(
           onPressed: () {
             context.read<GlobalDateCubit>().setPreviousMonthDates();
           },
-          icon: HugeIcon(
-            icon: HugeIcons.strokeRoundedArrowRightDouble,
-            color: Theme.of(context).colorScheme.primaryContainer,
+          child: Row(
+            children: [
+              Text(previousMonth, style: TextStyle(fontSize: 8)),
+              HugeIcon(
+                icon: HugeIcons.strokeRoundedArrowRightDouble,
+                color: Theme.of(context).colorScheme.primaryContainer,
+              ),
+            ],
           ),
         ),
+
         Spacer(),
         if (now.year != state.thisMonthDates.first.year ||
             now.month != state.thisMonthDates.first.month)
@@ -43,7 +89,7 @@ class AppChangeDate extends StatelessWidget {
             onPressed: () {
               context.read<GlobalDateCubit>().setThisMonthDates();
             },
-            child: Text('Current Month'),
+            child: Text('Current Month', style: TextStyle(fontSize: 12)),
           ),
       ],
     );
