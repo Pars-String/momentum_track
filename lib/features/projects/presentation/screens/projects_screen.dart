7f7a0080c3c29f0ffbc9157963f3ae18b87509a9
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-04 23:40:20 +0330

    fix: add more space at the bottom of the projects list

--- lib/features/projects/presentation/screens/projects_screen.dart
+++ lib/features/projects/presentation/screens/projects_screen.dart
@@ -55,6 +55,7 @@ class _ProjectsScreenState extends State<ProjectsScreen> {
                 StreakTracker(),
                 SliverGap(12),
                 ProjectsList(),
+                SliverGap(85),
               ],
             ),
           );
