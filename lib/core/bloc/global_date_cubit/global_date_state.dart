88653ee1b3f88017e8991219afb75133bf8edc91
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2025-09-18 10:03:23 +0330

    feat: add today info on global date cubit

--- lib/core/bloc/global_date_cubit/global_date_state.dart
+++ lib/core/bloc/global_date_cubit/global_date_state.dart
@@ -3,21 +3,25 @@ part of 'global_date_cubit.dart';
 enum DateListStatus { initial, loading, success, failure }
 
 class GlobalDateState extends Equatable {
+  final DateTime today;
   final List<DateTime> thisMonthDates;
   final DateListStatus dateListStatus;
   const GlobalDateState({
-    this.thisMonthDates = const [],
-    this.dateListStatus = DateListStatus.initial,
+    required this.today,
+    required this.thisMonthDates,
+    required this.dateListStatus,
   });
 
   @override
-  List<Object> get props => [thisMonthDates, dateListStatus];
+  List<Object> get props => [thisMonthDates, dateListStatus, today];
 
   GlobalDateState copyWith({
+    DateTime? today,
     List<DateTime>? thisMonthDates,
     DateListStatus? dateListStatus,
   }) {
     return GlobalDateState(
+      today: today ?? this.today,
       thisMonthDates: thisMonthDates ?? this.thisMonthDates,
       dateListStatus: dateListStatus ?? this.dateListStatus,
     );
